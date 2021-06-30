864326d2b450fcfdae6408273e1fb95161758ab0
janeth <vargasjaneth984@gmail.com>
2021-06-30 19:00:59 +0200

    actualizacion del app

--- app.py
+++ app.py
@@ -1,79 +1,143 @@
+import base64
+import datetime
+import io
+import plotly.express as px
 import dash
-import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
-from dash_html_components.P import P
 import dash_table
 
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+import pandas as pd
 
-# the style arguments for the sidebar. We use position:fixed and a fixed width
-SIDEBAR_STYLE = {
-    "position": "fixed",
-    "top": 0,
-    "left": 0,
-    "bottom": 0,
-    "width": "16rem",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
-}
 
-# the styles for the main content position it to the right of the sidebar and
-# add some padding.
-CONTENT_STYLE = {
-    "margin-left": "18rem",
-    "margin-right": "2rem",
-    "padding": "2rem 1rem",
-}
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-sidebar = html.Div(
-    [
-        html.H2("Sidebar", className="display-4"),
-        html.Hr(),
-        html.P(
-            "A simple sidebar layout with navigation links", className="lead"
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets,title='Dashboard_file')
+
+app.layout = html.Div([
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'SUBA SU FICHERO ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '50%',
+            'height': '30px',
+            'lineHeight': '30px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    html.Div(id='output-data-upload'),
+    #lo nueevo
+    html.P("Names:"),
+    dcc.Dropdown(
+        id='upload-names' , 
+        value='desc_finalidad', 
+        options=[{'value': x, 'label': x} 
+                 for x in ['RECUP. SUBASTA;', 'GARANTIA HIPOTECARIA', 'RET. CIRCULAR BDE VMER', 'CIRCULAR BDE']],
+        clearable=False
+    ),
+    html.P("Values:"),
+    dcc.Dropdown(
+        id='upload-values',
+        value='cod_finalidad', 
+        options=[{'value': x, 'label': x} 
+                 for x in [15, 17, 1,19]],
+        clearable=False
+    ),
+    dcc.Graph(id='output-pie-chart-upload',
+              
+        )
+])
+
+
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')),sep=';')
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        dash_table.DataTable(
+            #id= 'table',
+            data=df.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in  df.columns],
+            fixed_rows={ 'headers': True, 'data': 0 },
+            style_cell=dict( whiteSpace='normal',
+                             line_color='darkslategray',
+                            #line_color= df.Color, fill_color=df.Color),
+                             fill_color='lightcyan'),
+                            
+            style_header=dict(backgroundColor="paleturquoise",
+                              line_color='darkslategray',
+                              fill_color='royalblue',
+                              align=['left','center'],
+                              font=dict(color='white', size=12),),
+            style_data=dict(backgroundColor="lavender")
+                         
         ),
-        dbc.Nav(
-            [
-                dbc.NavLink("Home", href="/", active="exact"),
-                dbc.NavLink("Page 1", href="/page-1", active="exact"),
-                dbc.NavLink("Page 2", href="/page-2", active="exact"),
-            ],
-            vertical=True,
-            pills=True,
-        ),
-    ],
-    style=SIDEBAR_STYLE,
-)
 
-content = html.Div(id="page-content", style=CONTENT_STYLE)
+        html.Hr(),  # horizontal line
 
-app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
-
-@app.callback(Output('datatable-upload-container', 'data'),
-              Output('datatable-upload-container', 'columns'),
-              Input('datatable-upload', 'contents'),
-              State('datatable-upload', 'filename'))
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
 
 
-@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
-def render_page_content(pathname):
-    if pathname == "/":
-        return html.P("This is the content of page ptivvipals. Yay!")
-    elif pathname == "/page-1":
-        return html.P("This is the content of page 1. Yay!")
-    elif pathname == "/page-2":
-        return html.P("Oh cool, this is page 2!")
-    # If the user tries to reach a different page, return a 404 message
-    return dbc.Jumbotron(
-        [
-            html.H1("404: Not found", className="text-danger"),
-            html.Hr(),
-            html.P(f"The pathname {pathname} was not recognised..."),
-        ]
-    )
+@app.callback(Output('output-data-upload', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified'))
+
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return  children 
 
 
-if __name__ == "__main__":
-    app.run_server(port=8888)
+df = update_output(Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified')) 
+
+
+@app.callback( Output('output-pie-chart-upload', "figure"), 
+               
+               Input('upload-names', 'value'),
+               Input('upload-values', 'value'))
+    
+
+def generate_chart(names, values):
+    fig = px.pie( df,values=values, names=names)
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True,port='8005')
--- codigode plotlydashboard.py
+++ codigode plotlydashboard.py
@@ -23,18 +23,16 @@ app.layout = html.Div([
             'width': '50%', 'height': '30px', 'lineHeight': '30px',
             'borderWidth': '1px', 'borderStyle': 'dashed',
             'borderRadius': '5px', 'textAlign': 'center', 'margin': '10px'},
-        multiple=True
     ),
-    dash_table.DataTable(id='datatable-upload-container',
-                         
-                         page_current=0,
-                         page_size=PAGE_SIZE,
-                         page_action='custom',
-                         style_cell={
-                             'whiteSpace': 'normal',
-                             'height': 'auto', },
-                         ),
-
+    dash_table.DataTable(id='datatable-upload-container',        
+                        page_current=0,
+                        page_size=PAGE_SIZE,
+                        page_action='custom',
+                        style_cell={
+                                    'whiteSpace': 'normal',
+                                    'height': 'auto',},
+    ),
+   
     dcc.Checklist(
         id='datatable-use-page-count',
         options=[
@@ -51,67 +49,61 @@ app.layout = html.Div([
         value=20
     ),
     dcc.Graph(id='datatable-upload-graph')
-
+    
 ])
-
 
 
 def parse_contents(contents, filename):
     content_type, content_string = contents.split(',')
     decoded = base64.b64decode(content_string)
     print(filename)
-    try:
-       if 'csv' in filename:
+    if 'csv' in filename:
         # Assume that the user uploaded a CSV file
-          df=pd.read_csv(
+        return pd.read_csv(
             io.StringIO(decoded.decode('utf-8')))
-       elif 'xls' in filename:
+    elif 'xls' in filename:
         # Assume that the user uploaded an excel file
-          df=pd.read_excel(io.BytesIO(decoded))
+        return pd.read_excel(io.BytesIO(decoded))
 
-    except Exception as e:
-        print(e)
-        return None
-        
+
+@app.callback(Output('datatable-upload', 'contents'),
+              Input('datatable-upload', 'contents'),
+              State('datatable-upload', 'filename')
+              )
+def update_output(contents, filename):
+    if contents is None:
+        return [{}], []
+    df = parse_contents(contents, filename)
 
     return df
-    
+
 
 @app.callback(Output('datatable-upload-container', 'data'),
-             Output('datatable-upload-container', 'columns'),
-             Input('datatable-upload', 'contents'),
-              Input('datatable-upload', 'filename'),)
-
-def update_output( contents, filename,):
-    start_table_df = pd.DataFrame(columns=['Start Column'])  ####### inserted line
-    if contents and filename:
-        df_nuevo = parse_contents(contents, filename,)
-        data=start_table_df.to_dict('records')
-        columns=[{"name": i, "id": i} for i in  sorted(df_nuevo.columns)]
-        
-        print(df_nuevo.head())
-    return data,columns
-    #else:
- 
-        #return start_table_df.to_dict('records'), [{'id': '', 'name': ''}]
-
-        
+              Output('datatable-upload-container', 'columns'),
+    Input('datatable-upload-container', "page_current"),
+    Input('datatable-upload-container', "page_size"),
+    Input('datatable-upload-container', 'sort_by'),
+    Input('datatable-upload', 'filename'))
 
 
-@app.callback(
-    Output('datatable-upload-container', 'page_count'),
-    Input('datatable-use-page-count', 'value'),
-    Input('datatable-page-count', 'value'))
-def update_output(use_page_count, page_count_value):
-    if len(use_page_count) == 0 or page_count_value is None:
-        return None
-    return page_count_value
+
+def update_table(page_current, page_size,filename,):
+    df = pd.read_csv(filename)
+    
+    return df.iloc[page_current*page_size:(page_current+ 1)*page_size].to_dict('records')
 
 
 @app.callback(Output('datatable-upload-graph', 'figure'),
               Input('datatable-upload-container', 'data'))
+
+
+
 def display_graph(rows):
     df = pd.DataFrame(rows)
+    page_current=0,
+    page_size=PAGE_SIZE,
+    page_action='custom'
+
     if (df.empty or len(df.columns) < 1):
         return {
             'data': [{
@@ -125,10 +117,10 @@ def display_graph(rows):
             'x': df[df.columns[0]],
             'y': df[df.columns[1]],
             'type': 'bar',
-
+            
         }]
     }
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port='8888')
+    app.run_server(debug=True,port='8888')
